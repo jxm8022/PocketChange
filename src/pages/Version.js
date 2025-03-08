8c55283a580d2b998ac737a71a0a1f89d5c680e7
Jose Mendoza <jose.mendoza5@mavs.uta.edu>
2025-03-07 23:21:25 -0500

    Update version page and data

--- src/pages/Version.js
+++ src/pages/Version.js
@@ -3,12 +3,21 @@ import versionHistory from "../assets/versionHistory";
 import Template from "../components/UI/Template/Template";
 
 const Version = () => {
+    const getVersionInfo = (info) => {
+        const link = info.link ? <a rel='noreferrer noopener' href={info.link} target='_blank'>link</a> : <></>;
+        return <li key={info.id} className="version-info">
+            {info.title}{info.link ? ' - ' : ''}{link}
+        </li>
+    }
+
     return (
         <Template>
             <h1>{labels.versionHistory}</h1>
             {versionHistory.map((version) => <div className="version-body" key={version.id}>
                 <h3 className="version-title">v{version.version} - {version.title}</h3>
-                {version.info.map((info) => <p key={info.id} className="version-info">{info.title} - <a rel='noreferrer noopener' href={info.link} target='_blank'>issue</a></p>)}
+                <ul>
+                    {version.info.map((info) => getVersionInfo(info))}
+                </ul>
             </div>)}
         </Template>
     );
